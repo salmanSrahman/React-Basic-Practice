0134be5d41c55831f32ecccd54d80e728b3da4ef
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-19 20:47:22 +0600

    google authenitication by firebase

--- simple-auth/src/Components/GoogleSign/GoogleSign.js
+++ simple-auth/src/Components/GoogleSign/GoogleSign.js
@@ -1,18 +1,38 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
-import { getAuth, getRedirectResult, GoogleAuthProvider } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 import firebaseAuthentication from "../../Firebase/Firebase.init";
 
 firebaseAuthentication();
 
 const GoogleSign = () => {
+  const [user, setUser] = useState({});
+
   const handleGoogSign = () => {
     const auth = getAuth();
-    const googlepPovider = new GoogleAuthProvider();
+    const googleProvider = new GoogleAuthProvider();
+    signInWithPopup(auth, googleProvider).then((result) => {
+      const { displayName, email, photoURL } = result.user;
+      console.log(result.user);
+      const loggedUser = {
+        name: displayName,
+        email: email,
+        img: photoURL,
+      };
+      setUser(loggedUser);
+    });
   };
   return (
     <div className="mt-5">
       <Button onClick={handleGoogSign}>Google Sign In</Button>
+      <br />
+      {user.email && (
+        <div>
+          <h3>{user.name}</h3>
+          <h3>{user.email}</h3>
+          <img src={user.img} alt="" />
+        </div>
+      )}
     </div>
   );
 };
