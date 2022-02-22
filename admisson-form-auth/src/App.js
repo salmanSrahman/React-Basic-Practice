cea2fe69bafcddc1155acab18df2221b2ab6fbf4
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-22 13:10:30 +0600

    admission form authentication

---
+++ admisson-form-auth/src/App.js
@@ -0,0 +1,13 @@
+import "./App.css";
+import "bootstrap/dist/css/bootstrap.min.css";
+import AdmissionForm from "./Components/AdmissionForm/AdmissionForm";
+
+function App() {
+  return (
+    <div>
+      <AdmissionForm></AdmissionForm>
+    </div>
+  );
+}
+
+export default App;
---
+++ admisson-form-auth/src/Components/AdmissionForm/AdmissionForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./AdmissionForm.css";
+
+const AdmissionForm = () => {
+  return (
+    <div>
+      <h1>Admission Form</h1>
+    </div>
+  );
+};
+
+export default AdmissionForm;
