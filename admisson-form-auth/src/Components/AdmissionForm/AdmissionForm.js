31ada868d0887f6c97477a866ee021d664659a49
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-22 16:33:52 +0600

    admission form authentication

--- admisson-form-auth/src/Components/AdmissionForm/AdmissionForm.js
+++ admisson-form-auth/src/Components/AdmissionForm/AdmissionForm.js
@@ -1,28 +1,55 @@
-import React from "react";
+import React, { useState } from "react";
 import { Button, Container, Form } from "react-bootstrap";
 import "./AdmissionForm.css";
 
 const AdmissionForm = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+  };
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+  };
+
+  const handleRegistration = (e) => {
+    e.preventDefault();
+    console.log(email, password);
+  };
+
   return (
     <Container>
-      <Form>
-        <Form.Group className="mb-3" controlId="formGroupEmail">
-          <Form.Label>Name</Form.Label>
-          <Form.Control type="text" placeholder="Enter Name" />
-        </Form.Group>
+      <Form onSubmit={handleRegistration}>
+        <Form.Text id="passwordHelpBlock" className="fs-1 text-dark fw-lighter">
+          Please Register
+        </Form.Text>
+
         <Form.Group className="mb-3" controlId="formGroupEmail">
           <Form.Label>Email address</Form.Label>
-          <Form.Control type="email" placeholder="Enter email" />
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            onBlur={handleEmail}
+            required
+          />
         </Form.Group>
         <Form.Group className="mb-3" controlId="formGroupPassword">
           <Form.Label>Password</Form.Label>
-          <Form.Control type="password" placeholder="Password" />
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            onBlur={handlePassword}
+            required
+          />
         </Form.Group>
         <Form.Group className="mb-3" id="formGridCheckbox">
           <Form.Check type="checkbox" label="Check me out" />
         </Form.Group>
-        <Button variant="primary">Hello</Button>
-        <Button variant="danger" className="ms-2">
+        <Button variant="primary" type="submit">
+          Registration
+        </Button>
+        <Button variant="danger" type="submit" className="ms-2">
           Reset Password
         </Button>
       </Form>
