11bb041a1e8c7cbccd1957a1f5883f37cec70317
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-23 11:45:37 +0600

    search option

--- rest-country/src/Components/Countries/Countries.js
+++ rest-country/src/Components/Countries/Countries.js
@@ -7,6 +7,17 @@ const Countries = () => {
   const [countries, setCountries] = useState([]);
   const [displaySearch, setDisplaySearch] = useState([]);
 
+
+
+  const handleSearch = (e) => {
+    const searchText = e.target.value;
+    const searchCountry = countries.filter((country) =>
+      country.name.common.toLowerCase().includes(searchText.toLowerCase())
+    );
+    setDisplaySearch(searchCountry);
+  };
+
+
   useEffect(() => {
     fetch("https://restcountries.com/v3.1/all")
       .then((res) => res.json())
@@ -16,13 +27,13 @@ const Countries = () => {
       });
   }, [countries]);
 
-  const handleSearch = (e) => {
-    const searchText = e.target.value;
-    const searchCountry = countries.filter((country) =>
-      country.name.toLowerCase().includes(searchText.toLowerCase())
-    );
-    setDisplaySearch(searchCountry);
-  };
+  // const handleSearch = (e) => {
+  //   const searchText = e.target.value;
+  //   const searchCountry = countries.filter((country) =>
+  //     country.name.common.toLowerCase().includes(searchText.toLowerCase())
+  //   );
+  //   setDisplaySearch(searchCountry);
+  // };
 
   return (
     <>
@@ -37,7 +48,7 @@ const Countries = () => {
         <Container>
           <Row xs={1} md={4} className="g-3">
             {displaySearch.map((country) => (
-              <Country key={country.tld} country={country}></Country>
+              <Country country={country}></Country>
             ))}
           </Row>
         </Container>
