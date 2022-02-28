9f641538c1cca2bc93bbb29b7563ce7f683375d1
Salman-S-Rahman <salmanmern@gmail.com>
2022-02-28 10:54:14 +0600

    header part end

--- ema-john-prc/src/Components/Header/Header.js
+++ ema-john-prc/src/Components/Header/Header.js
@@ -1,10 +1,17 @@
 import React from "react";
 import "./Header.css";
+import logo from "../../images/logo.png";
 
 const Header = () => {
   return (
-    <div>
-      <h1>Header</h1>
+    <div className="header-part">
+      <img src={logo} alt="" />
+      <nav className="nav-part">
+        <a href="/">Home</a>
+        <a href="/">Shop</a>
+        <a href="/">Order Review</a>
+        <a href="/">Manage Inventory</a>
+      </nav>
     </div>
   );
 };
