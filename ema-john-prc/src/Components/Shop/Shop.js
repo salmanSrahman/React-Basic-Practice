339a56beccee80444ce9157612fc1fde679b1cb4
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-02 15:26:28 +0600

    add to local storage

--- ema-john-prc/src/Components/Shop/Shop.js
+++ ema-john-prc/src/Components/Shop/Shop.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from "react";
 import Cart from "../Cart/Cart";
 import Product from "../Product/Product";
 import "./Shop.css";
+import { addToDb } from "../../utilities/fakedb";
 
 const Shop = () => {
   const [products, setProducts] = useState([]);
@@ -16,6 +17,7 @@ const Shop = () => {
   const handleAddToCart = (product) => {
     const newCart = [...cart, product];
     setCart(newCart);
+    addToDb(product.key);
   };
 
   return (
