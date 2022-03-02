3349d2f828cd8f177ea31fb64dc0d5f81bf5123c
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-02 20:01:34 +0600

    simple form auth

---
+++ simple-auth1/src/App.js
@@ -0,0 +1,12 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import FormAuth from "./Components/FormAuth/FormAuth";
+
+function App() {
+  return (
+    <div>
+      <FormAuth></FormAuth>
+    </div>
+  );
+}
+
+export default App;
---
+++ simple-auth1/src/Components/FormAuth/FormAuth.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const formAuth = () => {
+  return (
+    <div>
+      <h1>Form Auth</h1>
+    </div>
+  );
+};
+
+export default formAuth;
