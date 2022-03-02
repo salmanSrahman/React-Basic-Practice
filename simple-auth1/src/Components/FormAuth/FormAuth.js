ac9be836935792a0213305c6b2fed9ed91c09438
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-02 21:01:56 +0600

    google sign in cpmpletre

--- simple-auth1/src/Components/FormAuth/FormAuth.js
+++ simple-auth1/src/Components/FormAuth/FormAuth.js
@@ -1,9 +1,26 @@
 import React from "react";
+import { Button } from "react-bootstrap";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import firebaseAuthentication from "../Firebase/Firebase.Init";
+
+firebaseAuthentication();
 
 const formAuth = () => {
+  const goggleProvider = new GoogleAuthProvider();
+  const auth = getAuth();
+
+  const handleGoogleSign = () => {
+    signInWithPopup(auth, goggleProvider)
+    .then((result) => {
+      const user = result.user;
+      console.log(user);
+    });
+  };
+
   return (
     <div>
       <h1>Form Auth</h1>
+      <Button onClick={handleGoogleSign}>Google Sign</Button>
     </div>
   );
 };
