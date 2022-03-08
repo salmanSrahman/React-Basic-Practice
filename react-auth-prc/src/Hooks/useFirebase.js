bc119a6c900b4d747782c97bccd96c9b32f12225
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-08 18:08:16 +0600

    google sign in added

---
+++ react-auth-prc/src/Hooks/useFirebase.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+import firebaseInitialization from "../Firebase/Firebase.init";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+
+firebaseInitialization();
+const useFirebase = () => {
+  const [user, setUser] = useState({});
+  const [error, setError] = useState("");
+
+  const auth = getAuth();
+  const googleProvider = new GoogleAuthProvider();
+  const signInUsingGoogle = () => {
+    signInWithPopup(auth, googleProvider)
+      .then((result) => {
+        setUser(result.user);
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+
+  return {
+    user,
+    error,
+    signInUsingGoogle,
+  };
+};
+
+export default useFirebase;
