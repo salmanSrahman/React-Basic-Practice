84768138898661a775a60b906dea938a2b9277ef
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-08 13:08:41 +0600

    connected with firebase

---
+++ react-auth-prc/src/Firebase/Firebase.config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAYpJlg3wmksVFD3cmQMcATb_QnocSH0fs",
+  authDomain: "simple-auth1.firebaseapp.com",
+  projectId: "simple-auth1",
+  storageBucket: "simple-auth1.appspot.com",
+  messagingSenderId: "784832597834",
+  appId: "1:784832597834:web:46d0c7e08fb6da1abf3bb3",
+};
+
+export default firebaseConfig;
---
+++ react-auth-prc/src/Firebase/Firebase.init.js
@@ -0,0 +1,7 @@
+import { initializeApp } from "firebase/app";
+import firebaseConfig from "./Firebase.config";
+
+const firebaseInitialization = () => {
+  initializeApp(firebaseConfig);
+};
+export default firebaseInitialization;
