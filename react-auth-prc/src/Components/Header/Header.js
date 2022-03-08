293816de43d2315c802860932e1f9946df1713f9
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-08 19:31:32 +0600

    goggle sign in used

--- react-auth-prc/src/Components/Header/Header.js
+++ react-auth-prc/src/Components/Header/Header.js
@@ -1,15 +1,17 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import useFirebase from "../../Hooks/useFirebase";
 import "./Header.css";
 
 const Header = () => {
+  const { user } = useFirebase();
   return (
     <div className="header-part">
       <Link to="/home">Home</Link>
       <Link to="/about">About</Link>
       <Link to="/register">Register</Link>
       <Link to="/login">Login</Link>
-      <button>Logout</button>
+      {user?.email && <button>Logout</button>}
     </div>
   );
 };
