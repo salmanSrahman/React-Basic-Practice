3d2b55c0747718029bfd1e6a43e942f0e95b9ffb
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-09 09:47:50 +0600

    authentication practice

---
+++ simple-auth2/src/Components/About/About.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./About.css";
+
+const About = () => {
+  return (
+    <div>
+      <h1>This is all about us.</h1>
+    </div>
+  );
+};
+
+export default About;
---
+++ simple-auth2/src/Components/Registration/Registration.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./Registration.css";
+
+const Registration = () => {
+  return (
+    <div>
+      <h1>Here is registration.</h1>
+    </div>
+  );
+};
+
+export default Registration;
