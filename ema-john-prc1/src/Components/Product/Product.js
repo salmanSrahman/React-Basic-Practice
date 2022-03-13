1de7b1cfd7fa743ff6604789c4023bb44183897c
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-14 03:35:06 +0600

    all products displayed in ui

--- ema-john-prc1/src/Components/Product/Product.js
+++ ema-john-prc1/src/Components/Product/Product.js
@@ -10,7 +10,11 @@ const Product = ({ product }) => {
         <img src={img} alt="" />
       </div>
       <div className="product-details">
-        <h2> {name}</h2>
+        <h3 style={{ color: "blue" }}> {name}</h3>
+        <h5>By: {seller}</h5>
+        <h5>Price: {price}</h5>
+        <h5>Category :{category}</h5>
+        <h5>only {stock} left in stock - order soon</h5>
       </div>
     </div>
   );
