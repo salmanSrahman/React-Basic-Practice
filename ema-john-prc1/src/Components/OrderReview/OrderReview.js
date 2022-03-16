da1404e611109acdb0b81ef5d88d897d4ab1a4fc
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-16 11:59:25 +0600

    used hooks

--- ema-john-prc1/src/Components/OrderReview/OrderReview.js
+++ ema-john-prc1/src/Components/OrderReview/OrderReview.js
@@ -1,9 +1,13 @@
 import React from "react";
+import useProduct from "../../useHooks/useProduct";
 import "./OrderReview.css";
 
 const OrderReview = () => {
+  const [products] = useProduct();
+
   return (
     <div>
+      {products.length}
       <h1>Here is order review.</h1>
     </div>
   );
