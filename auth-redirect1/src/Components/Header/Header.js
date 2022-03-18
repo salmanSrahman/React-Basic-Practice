489f59c8dba668f31039c401e247f0038ec8a968
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-18 18:41:06 +0600

    sign out added

--- auth-redirect1/src/Components/Header/Header.js
+++ auth-redirect1/src/Components/Header/Header.js
@@ -1,13 +1,22 @@
 import React from "react";
+import { Button } from "react-bootstrap";
 import { Link } from "react-router-dom";
-import './Header.css'
+import useFirebase from "../../Hooks/useFirebase";
+import "./Header.css";
 
 const Header = () => {
+  const { user, logOut} = useFirebase();
+
   return (
     <div className="header-part">
       <Link to="/home">Home</Link>
       <Link to="/register">Register</Link>
       <Link to="/login">Login</Link>
+      {user?.email && (
+        <Button variant="outline-danger" size="sm" onClick={logOut}>
+          Log Out
+        </Button>
+      )}
     </div>
   );
 };
--- auth-redirect1/src/Hooks/useFirebase.js
+++ auth-redirect1/src/Hooks/useFirebase.js
@@ -1,4 +1,10 @@
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import {
+  getAuth,
+  signInWithPopup,
+  GoogleAuthProvider,
+  signOut,
+  onAuthStateChanged,
+} from "firebase/auth";
 import { useState } from "react";
 import { firebaseInitialization } from "../Firebase/Firebase.init";
 
@@ -6,16 +12,38 @@ firebaseInitialization();
 
 const useFirebase = () => {
   const [user, setUser] = useState({});
+  const [error, setError] = useState("");
   const GoogleProvider = new GoogleAuthProvider();
   const auth = getAuth();
 
   const useGoogleSignIn = () => {
-    signInWithPopup(auth, GoogleProvider).then((result) => {
-      setUser(result.user);
-    });
+    signInWithPopup(auth, GoogleProvider)
+      .then((result) => {
+        setUser(result.user);
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
   };
 
-  return { user, useGoogleSignIn };
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      setUser(user);
+    } else {
+    }
+  });
+
+  const logOut = () => {
+    signOut(auth)
+      .then(() => {
+        setUser({});
+      })
+      .catch((error) => {
+        setError(error.message);
+      });
+  };
+
+  return { user, error, useGoogleSignIn, logOut };
 };
 
 export default useFirebase;
