186a8bc5d6188d8e6806cfac9da37e3af6683c7d
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-20 21:11:47 +0600

    private route applied

--- private-route1/src/App.js
+++ private-route1/src/App.js
@@ -8,6 +8,7 @@ import Shipping from "./Components/Shipping/Shipping";
 import Header from "./Components/Header/Header";
 import { Container } from "react-bootstrap";
 import AuthProvider from "./Components/Context/AuthProvider";
+import PrivateRoute from "./Components/PrivateRoute/PrivateRoute";
 
 function App() {
   return (
@@ -19,7 +20,14 @@ function App() {
             <Routes>
               <Route path="/" element={<Home />}></Route>
               <Route path="home" element={<Home />}></Route>
-              <Route path="shipping" element={<Shipping />}></Route>
+              <Route
+                path="shipping"
+                element={
+                  <PrivateRoute>
+                    <Shipping />
+                  </PrivateRoute>
+                }
+              ></Route>
               <Route path="register" element={<Register />}></Route>
               <Route path="login" element={<LogIn />}></Route>
             </Routes>
