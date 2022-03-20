38e7dd15156c5a2636ed7f0d21d27ebfd9383018
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-20 19:51:13 +0600

    created context api

---
+++ private-route1/src/Components/Context/AuthProvider.js
@@ -0,0 +1,13 @@
+import React, { createContext } from "react";
+import useFirebase from "../../Hooks/useFirebase";
+
+const AuthContext = createContext();
+
+const AuthProvider = ({ children }) => {
+  const AllContext = useFirebase();
+  return (
+    <AuthContext.Provider value={AllContext}>{children}</AuthContext.Provider>
+  );
+};
+
+export default AuthProvider;
