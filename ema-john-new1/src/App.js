a7ca2c525bff94269e982082ed522896250daac9
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-25 12:12:37 +0600

    all routes set up done

--- ema-john-new1/src/App.js
+++ ema-john-new1/src/App.js
@@ -1,9 +1,26 @@
-import './App.css';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./Components/Home/Home";
+import Header from "./Components/Header/Header";
+import Shop from "./Components/Shop/Shop";
+import Order from "./Components/Order/Order";
+import Inventory from "./Components/Inventory/Inventory";
+import NotFound from "./Components/NotFound/NotFound";
 
 function App() {
   return (
     <div>
-     
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />}></Route>
+          <Route path="home" element={<Home />}></Route>
+          <Route path="shop" element={<Shop />}></Route>
+          <Route path="order" element={<Order />}></Route>
+          <Route path="inventory" element={<Inventory />}></Route>
+          <Route path="notfound" element={<NotFound />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
