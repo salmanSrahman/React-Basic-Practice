8f60529fefd42e5dc934e2afaaeec8593c18040a
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-27 23:59:08 +0600

    all products shown

--- computer-shop/src/Components/Product/Product.js
+++ computer-shop/src/Components/Product/Product.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Button, Col } from "react-bootstrap";
+import Rating from "react-rating";
 import "./Product.css";
 
 const Product = ({ product }) => {
@@ -12,6 +13,15 @@ const Product = ({ product }) => {
           <img src={img} alt="" className="img-fluid" />
           <h4>{name}</h4>
           <h5>Price: ${price}</h5>
+          <div>
+            <Rating
+              className="rating-style"
+              initialRating={star}
+              emptySymbol="fa fa-star-o fa-2x"
+              fullSymbol="fa fa-star fa-2x"
+              readonly
+            />
+          </div>
           <h5>
             Only {stock} left in stock - <span>order soon</span>
           </h5>
