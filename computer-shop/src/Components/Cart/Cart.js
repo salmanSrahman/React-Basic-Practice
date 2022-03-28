d86ee8b343134e8f27aa21ac29d7e2a9d05dbdea
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-28 23:36:28 +0600

    cart details component added

--- computer-shop/src/Components/Cart/Cart.js
+++ computer-shop/src/Components/Cart/Cart.js
@@ -1,9 +1,15 @@
 import React from "react";
+import CartDetails from "../CartDetails/CartDetails";
 
 const Cart = ({ cart }) => {
   return (
     <div>
       <h3>Selected Product: {cart.length} </h3>
+      <div>
+        {cart.map((product) => (
+          <CartDetails key={product.key} product={product}></CartDetails>
+        ))}
+      </div>
     </div>
   );
 };
---
+++ computer-shop/src/Components/CartDetails/CartDetails.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./CartDetails.css";
+
+const CartDetails = () => {
+  return (
+    <div>
+      <h1>Here is cart details.</h1>
+    </div>
+  );
+};
+
+export default CartDetails;
