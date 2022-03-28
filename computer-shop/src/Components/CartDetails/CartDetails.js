3d54460d9ea1c24787edaa4b294def5642960e99
Salman-S-Rahman <salmanmern@gmail.com>
2022-03-28 23:48:44 +0600

    cart details component added

--- computer-shop/src/Components/CartDetails/CartDetails.js
+++ computer-shop/src/Components/CartDetails/CartDetails.js
@@ -1,10 +1,23 @@
 import React from "react";
 import "./CartDetails.css";
+import { AiTwotoneDelete } from "react-icons/ai";
 
-const CartDetails = () => {
+const CartDetails = ({ product }) => {
+  const { img, name, price } = product;
   return (
     <div>
-      <h1>Here is cart details.</h1>
+      <div className="d-flex justify-content-between">
+        <div>
+          <img src={img} alt="" className="img-fluid" />
+        </div>
+        <div>
+          <h6>{name}</h6>
+          <h6>${price}</h6>
+        </div>
+        <div>
+          <AiTwotoneDelete />
+        </div>
+      </div>
     </div>
   );
 };
