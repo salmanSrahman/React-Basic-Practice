d2e376799979259494b8a9a3b9d833c6bd871a5e
Salman-S-Rahman <salmanmern@gmail.com>
2022-04-06 23:56:11 +0600

    google sign in done

--- simple-auth-new1/src/App.js
+++ simple-auth-new1/src/App.js
@@ -1,9 +1,21 @@
 import "./App.css";
+import app from "./firebase.config";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 function App() {
+  const auth = getAuth(app);
+  const googleProvider = new GoogleAuthProvider();
+
+  const googleSignIn = () => {
+    signInWithPopup(auth, googleProvider).then((result) => {
+      console.log(result.user);
+    });
+  };
+
   return (
     <div className="App">
       <h1>Authentication</h1>
+      <button onClick={googleSignIn}>Google Sign In</button>
     </div>
   );
 }
