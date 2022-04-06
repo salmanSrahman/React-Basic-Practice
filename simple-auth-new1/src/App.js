21f540cb5d4a076a92ce6373a0794a6df6ed1f83
Salman-S-Rahman <salmanmern@gmail.com>
2022-04-06 23:58:24 +0600

    showed display name in ui

--- simple-auth-new1/src/App.js
+++ simple-auth-new1/src/App.js
@@ -1,20 +1,24 @@
 import "./App.css";
 import app from "./firebase.config";
 import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { useState } from "react";
 
 function App() {
+  const [user, setUser] = useState({});
   const auth = getAuth(app);
   const googleProvider = new GoogleAuthProvider();
 
   const googleSignIn = () => {
     signInWithPopup(auth, googleProvider).then((result) => {
       console.log(result.user);
+      setUser(result.user);
     });
   };
 
   return (
     <div className="App">
       <h1>Authentication</h1>
+      {user.displayName}
       <button onClick={googleSignIn}>Google Sign In</button>
     </div>
   );
