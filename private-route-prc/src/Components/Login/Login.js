f556399e9a994dda7521f941fd7f61c99fc68115
Salman-S-Rahman <salmanmern@gmail.com>
2022-04-11 12:06:42 +0600

    protected route with auth redirect

--- private-route-prc/src/Components/Login/Login.js
+++ private-route-prc/src/Components/Login/Login.js
@@ -3,21 +3,30 @@ import React from "react";
 import { Button, Container } from "react-bootstrap";
 import Form from "react-bootstrap/Form";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
+import { useLocation, useNavigate } from "react-router-dom";
 import app from "../../Firebase.config";
 
 const auth = getAuth(app);
 
 const Login = () => {
   const [signInWithGoogle] = useSignInWithGoogle(auth);
+
+  let navigate = useNavigate();
+  let location = useLocation();
+  let from = location?.state?.from?.pathname || "/";
+
+  const handleGoogleSign = () => {
+    signInWithGoogle()
+    .then(() => {
+    navigate(from, { replace: true });
+    })
+    
+  };
+
   return (
     <div>
       <Container>
-        <Button
-          onClick={() => {
-            signInWithGoogle();
-          }}
-          className="mx-auto"
-        >
+        <Button onClick={handleGoogleSign} className="mx-auto">
           Google SignIn
         </Button>
         <div className="w-50 mx-auto">
