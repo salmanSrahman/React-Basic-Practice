0de4cf94961ddaf5ed60ca87b7ec0e40a78090cf
Salman-S-Rahman <salmanmern@gmail.com>
2022-06-10 12:14:19 +0600

    remobe from db

--- data-load/src/Components/Product/Product.js
+++ data-load/src/Components/Product/Product.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Button, Col } from "react-bootstrap";
-import { addToDb } from "../../utilities/fakeDb";
+import { addToDb, removeFromDb } from "../../utilities/fakeDb";
 import "./Product.css";
 
 const Product = (props) => {
@@ -10,12 +10,19 @@ const Product = (props) => {
     addToDb(id);
   };
 
+  const removeFromCart = (id) => {
+    removeFromDb(id);
+  };
+
   return (
     <Col>
       <img src={img} alt="" className="img-fluid" />
       <h4>{name.slice(0, 18)}</h4>
       <h4>Price: {price}</h4>
       <Button onClick={() => addToCart(id)}>Buy This</Button>
+      <Button onClick={() => removeFromCart(id)} className="ms-2 btn-danger">
+        Remove This
+      </Button>
     </Col>
   );
 };
--- data-load/src/utilities/fakeDb.js
+++ data-load/src/utilities/fakeDb.js
@@ -1,13 +1,39 @@
-// add to local storage
-
 const addToDb = (id) => {
-  const quantity = localStorage.getItem(id);
+  let shoppingCart;
+
+  const storedCart = localStorage.getItem("shopping-cart");
+  if (storedCart) {
+    shoppingCart = JSON.parse(storedCart);
+  } else {
+    shoppingCart = {};
+  }
+
+  // quantity
+  const quantity = shoppingCart[id];
   if (quantity) {
     const newQuantity = quantity + 1;
-    localStorage.setItem(id, newQuantity);
+    shoppingCart[id] = newQuantity;
   } else {
-    localStorage.setItem(id, 1);
+    shoppingCart[id] = 1;
   }
+  localStorage.setItem("shopping-cart", JSON.stringify(shoppingCart));
 };
 
-export { addToDb };
+const removeFromDb = (id) => {
+  let shoppingCart;
+  const storedCart = localStorage.getItem("shopping-cart");
+  if (storedCart) {
+    shoppingCart = JSON.parse(storedCart);
+  } else {
+    shoppingCart = {};
+  }
+
+  // quantity
+  const quantity = shoppingCart[id];
+  if (quantity) {
+    delete shoppingCart[id];
+  }
+  localStorage.setItem("shopping-cart", JSON.stringify(shoppingCart));
+};
+
+export { addToDb, removeFromDb };
